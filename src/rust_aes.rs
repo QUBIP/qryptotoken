48e6ea1986b5ae51044f7d85364de17a4f6cdcd9
Daniel Luoma <luomadaniel95@gmail.com>
2024-09-04 14:45:47 +0300

    Add skeleton foe rust-aes

--- src/rust_aes.rs
+++ src/rust_aes.rs
@@ -17,3 +17,100 @@ use crate::object::{
 
 use crate::mechanism;
 use mechanism::*;
+
+use once_cell::sync::Lazy;
+use std::fmt::Debug;
+
+pub const MIN_AES_SIZE_BYTES: usize = 16; /* 128 bits */
+pub const MID_AES_SIZE_BYTES: usize = 24; /* 192 bits */
+pub const MAX_AES_SIZE_BYTES: usize = 32; /* 256 bits */
+pub const AES_BLOCK_SIZE: usize = 16;
+
+fn check_key_len(len: usize) -> KResult<()> {
+    match len {
+        16 | 24 | 32 => Ok(()),
+        _ => err_rv!(CKR_KEY_SIZE_RANGE),
+    }
+}
+
+#[derive(Debug)]
+pub struct AesKeyFactory {
+    attributes: Vec<ObjectAttr>,
+}
+
+impl AesKeyFactory {}
+
+impl ObjectFactory for AesKeyFactory {}
+
+impl CommonKeyFactory for AesKeyFactory {}
+
+impl SecretKeyFactory for AesKeyFactory {}
+
+static AES_KEY_FACTORY: Lazy<Box<dyn ObjectFactory>> =
+    Lazy::new(|| Box::new(AesKeyFactory::new()));
+
+#[derive(Debug)]
+struct AesMechanism {
+    info: CK_MECHANISM_INFO,
+}
+
+impl Mechanism for AesMechanism {}
+
+pub fn register(mechs: &mut Mechanisms, ot: &mut ObjectFactories) {
+    AesOperation::register_mechanisms(mechs);
+
+    ot.add_factory(ObjectType::new(CKO_SECRET_KEY, CKK_AES), &AES_KEY_FACTORY);
+}
+
+#[derive(Debug)]
+struct AesKey {}
+
+impl Drop for AesKey {}
+
+
+fn new_mechanism(flags: CK_FLAGS) -> Box<dyn Mechanism> {
+    Box::new(AesMechanism {
+        info: CK_MECHANISM_INFO {
+            ulMinKeySize: MIN_AES_SIZE_BYTES as CK_ULONG,
+            ulMaxKeySize: MAX_AES_SIZE_BYTES as CK_ULONG,
+            flags: flags,
+        },
+    })
+}
+
+
+#[derive(Debug)]
+struct AesParams {
+    iv: Vec<u8>,
+    maxblocks: u128,
+    ctsmode: u8,
+    datalen: usize,
+    aad: Vec<u8>,
+    taglen: usize,
+}
+
+#[derive(Debug)]
+struct AesOperation {
+    mech: CK_MECHANISM_TYPE,
+    key: AesKey,
+    params: AesParams,
+    finalized: bool,
+    in_use: bool,
+    ctx: EvpCipherCtx,
+    finalbuf: Vec<u8>,
+    blockctr: u128,
+}
+
+impl Drop for AesOperation {
+    fn drop(&mut self) {
+        self.finalbuf.zeroize()
+    }
+}
+
+impl AesOperation {}
+
+impl MechOperation for AesOperation {}
+
+impl Encryption for AesOperation {}
+
+impl Decryption for AesOperation {}
