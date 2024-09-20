94d83a2e87f3645c581e0b0986399b68d18c4a1c
Daniel Luoma <luomadaniel95@gmail.com>
2024-09-20 17:11:19 +0300

    Add aes-gcm encryption

--- src/rust_aes.rs
+++ src/rust_aes.rs
@@ -5,7 +5,7 @@ use crate::attribute;
 use crate::error;
 use crate::interface;
 use crate::object;
-use crate::{attr_element, err_rv};
+use crate::{cast_params, attr_element, err_rv, bytes_to_vec};
 
 use crate::attribute::{from_bytes, from_bool, from_ulong};
 use crate::error::{KError, KResult};
@@ -293,6 +293,12 @@ impl Drop for AesKey {
     }
 }
 
+fn object_to_raw_key(key: &Object) -> KResult<AesKey> {
+    let val = key.get_attr_as_bytes(CKA_VALUE)?;
+    check_key_len(val.len())?;
+    Ok(AesKey { raw: val.clone() })
+}
+
 
 fn new_mechanism(flags: CK_FLAGS) -> Box<dyn Mechanism> {
     Box::new(AesMechanism {
@@ -334,20 +340,221 @@ enum AesGcmCipher {
 }
 
 impl AesOperation {
-    fn register_mechanisms(mechs: &mut Mechanisms) {todo!()}
+    fn register_mechanisms(mechs: &mut Mechanisms) {
+        for ckm in &[
+            CKM_AES_GCM,
+            CKM_AES_KEY_WRAP,
+            CKM_AES_KEY_WRAP_KWP,
+        ] {
+            mechs.add_mechanism(
+                *ckm,
+                new_mechanism(
+                    CKF_ENCRYPT | CKF_DECRYPT | CKF_WRAP | CKF_UNWRAP,
+                ),
+            );
+        }
 
-    fn init_params(mech: &CK_MECHANISM) -> KResult<AesParams> {todo!()}
+        mechs.add_mechanism(CKM_AES_KEY_GEN, new_mechanism(CKF_GENERATE));
+    }
 
+    fn init_params(mech: &CK_MECHANISM) -> KResult<AesParams> {
+        match mech.mechanism {
+            #[cfg(False)]
+            CKM_AES_CCM => {
+                let params = cast_params!(mech, CK_CCM_PARAMS);
+                if params.ulNonceLen < 7 || params.ulNonceLen > 13 {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                let l = 15 - params.ulNonceLen;
+                if params.ulDataLen == 0
+                    || params.ulDataLen > (1 << (8 * l))
+                    || (params.ulDataLen + params.ulMACLen)
+                        > u64::MAX as CK_ULONG
+                {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                if params.ulAADLen > (u32::MAX - 1) as CK_ULONG {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                match params.ulMACLen {
+                    4 | 6 | 8 | 10 | 12 | 14 | 16 => (),
+                    _ => return err_rv!(CKR_MECHANISM_PARAM_INVALID),
+                }
+                Ok(AesParams {
+                    iv: bytes_to_vec!(params.pNonce, params.ulNonceLen),
+                    maxblocks: 0,
+                    ctsmode: 0,
+                    datalen: params.ulDataLen as usize,
+                    aad: bytes_to_vec!(params.pAAD, params.ulAADLen),
+                    taglen: params.ulMACLen as usize,
+                })
+            }
+            CKM_AES_GCM => {
+                let params = cast_params!(mech, CK_GCM_PARAMS);
+                if params.ulIvLen == 0 || params.ulIvLen > (1 << 32) - 1 {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                if params.ulAADLen > (1 << 32) - 1 {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                if params.ulTagBits > 128 {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                if params.ulIvLen < 1 || params.pIv == std::ptr::null_mut() {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+                Ok(AesParams {
+                    iv: bytes_to_vec!(params.pIv, params.ulIvLen),
+                    //maxblocks: 0,
+                    //ctsmode: 0,
+                    //datalen: 0,
+                    aad: bytes_to_vec!(params.pAAD, params.ulAADLen),
+                    taglen: (params.ulTagBits as usize + 7) / 8,
+                })
+            }
+            #[cfg(False)]
+            CKM_AES_CTR => {
+                let params = cast_params!(mech, CK_AES_CTR_PARAMS);
+                let iv = params.cb.to_vec();
+                let ctrbits = params.ulCounterBits as usize;
+                let mut maxblocks = 0u128;
+                if ctrbits < (AES_BLOCK_SIZE * 8) {
+                    /* FIXME: support arbitrary counterbits wrapping.
+                     * OpenSSL CTR mode is built to handle the whole IV
+                     * as a 128bit counter unconditionally.
+                     * For callers that want a smaller counterbit size all
+                     * we can do is to set a maximum number of blocks so
+                     * that the counter space does *not* wrap (because
+                     * openssl won't wrap it but proceed to increment the
+                     * octets part of the IV/Nonce). This means that for
+                     * applications that initialize the counter to a value
+                     * like 1 all will be fine, but application that
+                     * initialize the counter to a random value and expect
+                     * wrapping will see a failure instead of wrapping */
+                    maxblocks = (1 << ctrbits) - 1;
+                    let fulloctects = ctrbits / 8;
+                    let mut idx = 0;
+                    while fulloctects > idx {
+                        maxblocks -= (iv[15 - idx] as u128) << (idx * 8);
+                        idx += 1;
+                    }
+                    let part = ctrbits % 8;
+                    if part > 0 {
+                        maxblocks -= ((iv[15 - idx] as u128) & (part as u128))
+                            << (idx * 8);
+                    }
+                    if maxblocks == 0 {
+                        return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                    }
+                } else if ctrbits > (AES_BLOCK_SIZE * 8) {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+
+                Ok(AesParams {
+                    iv: iv,
+                    maxblocks: maxblocks,
+                    ctsmode: 0,
+                    datalen: 0,
+                    aad: Vec::new(),
+                    taglen: 0,
+                })
+            }
+            #[cfg(False)]
+            CKM_AES_CTS | CKM_AES_CBC | CKM_AES_CBC_PAD => {
+                if mech.ulParameterLen != (AES_BLOCK_SIZE as CK_ULONG) {
+                    return err_rv!(CKR_ARGUMENTS_BAD);
+                }
+                let mut ctsmode = 0u8;
+                if mech.mechanism == CKM_AES_CTS {
+                    ctsmode = 1u8;
+                }
+                Ok(AesParams {
+                    iv: bytes_to_vec!(mech.pParameter, mech.ulParameterLen),
+                    maxblocks: 0,
+                    ctsmode: ctsmode,
+                    datalen: 0,
+                    aad: Vec::new(),
+                    taglen: 0,
+                })
+            }
+            #[cfg(False)]
+            CKM_AES_ECB => Ok(AesParams {
+                iv: Vec::with_capacity(0),
+                maxblocks: 0,
+                ctsmode: 0,
+                datalen: 0,
+                aad: Vec::new(),
+                taglen: 0,
+            }),
+            #[cfg(False)]
+            #[cfg(not(feature = "fips"))]
+            CKM_AES_CFB8 | CKM_AES_CFB1 | CKM_AES_CFB128 | CKM_AES_OFB => {
+                if mech.ulParameterLen != (AES_BLOCK_SIZE as CK_ULONG) {
+                    return err_rv!(CKR_ARGUMENTS_BAD);
+                }
+                Ok(AesParams {
+                    iv: bytes_to_vec!(mech.pParameter, mech.ulParameterLen),
+                    maxblocks: 0,
+                    ctsmode: 0,
+                    datalen: 0,
+                    aad: Vec::new(),
+                    taglen: 0,
+                })
+            }
+            #[cfg(False)]
+            CKM_AES_KEY_WRAP => {
+                let iv = match mech.ulParameterLen {
+                    0 => Vec::new(),
+                    8 => bytes_to_vec!(mech.pParameter, mech.ulParameterLen),
+                    _ => return err_rv!(CKR_ARGUMENTS_BAD),
+                };
+                Ok(AesParams {
+                    iv: iv,
+                    maxblocks: 0,
+                    ctsmode: 0,
+                    datalen: 0,
+                    aad: Vec::new(),
+                    taglen: 0,
+                })
+            }
+            #[cfg(False)]
+            CKM_AES_KEY_WRAP_KWP => {
+                let iv = match mech.ulParameterLen {
+                    0 => Vec::new(),
+                    4 => bytes_to_vec!(mech.pParameter, mech.ulParameterLen),
+                    _ => return err_rv!(CKR_ARGUMENTS_BAD),
+                };
+                Ok(AesParams {
+                    iv: iv,
+                    maxblocks: 0,
+                    ctsmode: 0,
+                    datalen: 0,
+                    aad: Vec::new(),
+                    taglen: 0,
+                })
+            }
+            _ => err_rv!(CKR_MECHANISM_INVALID),
+        }
+    }
+
+    #[cfg(False)]
     fn init_cipher(
         mech: CK_MECHANISM_TYPE,
         key: &[u8],
     ) -> KResult<AesGcmCipher> {todo!()}
 
-    fn encrypt_initialize(&mut self) -> KResult<()> {todo!()}
-    
-    fn decrypt_initialize(&mut self) -> KResult<()> {todo!()}
-
-    fn encrypt_new(mech: &CK_MECHANISM, key: &Object) -> KResult<AesOperation> {todo!()} 
+    fn encrypt_new(mech: &CK_MECHANISM, key: &Object) -> KResult<AesOperation> {
+        Ok(AesOperation {
+            mech: mech.mechanism,
+            key: object_to_raw_key(key)?,
+            params: Self::init_params(mech)?,
+            finalized: false,
+            in_use: false,
+            //ctx: EvpCipherCtx::new()?,
+            finalbuf: Vec::new(),
+            //blockctr: 0,
+        })
+    }
 
     fn decrypt_new(mech: &CK_MECHANISM, key: &Object) -> KResult<AesOperation> {todo!()} 
 
@@ -357,13 +564,51 @@ impl AesOperation {
         mut keydata: Vec<u8>,
         output: CK_BYTE_PTR,
         output_len: CK_ULONG_PTR,
-    ) -> KResult<()> {todo!()}
+    ) -> KResult<()> {
+        let mut op = match Self::encrypt_new(mech, wrapping_key) {
+            Ok(o) => o,
+            Err(e) => {
+                keydata.zeroize();
+                return Err(e);
+            }
+        };
+
+        match mech.mechanism {
+            #[cfg(False)]
+            CKM_AES_CBC | CKM_AES_ECB => {
+                /* non-padding block modes needs 0 padding for the input */
+                let pad = keydata.len() % AES_BLOCK_SIZE;
+                if pad != 0 {
+                    keydata.resize(keydata.len() + AES_BLOCK_SIZE - pad, 0);
+                }
+            }
+            #[cfg(False)]
+            CKM_AES_CCM => {
+                /* Check the data length in CCM matches the provided data -- this is one-shot
+                 * operation only */
+                if op.params.datalen != keydata.len() {
+                    return err_rv!(CKR_MECHANISM_PARAM_INVALID);
+                }
+            }
+            _ => (),
+        }
+        let result = op.encrypt(&keydata, output, output_len);
+        keydata.zeroize();
+        result
+    }
     
     fn unwrap(
         mech: &CK_MECHANISM,
         wrapping_key: &Object,
         data: &[u8],
-    ) -> KResult<Vec<u8>> {todo!()}
+    ) -> KResult<Vec<u8>> {
+        let mut op = Self::decrypt_new(mech, wrapping_key)?;
+        let mut result = vec![0u8; data.len()];
+        let mut len = result.len() as CK_ULONG;
+        op.decrypt(data, result.as_mut_ptr(), &mut len)?;
+        unsafe { result.set_len(len as usize) };
+        Ok(result)
+    }
 }
 
 impl MechOperation for AesOperation {
@@ -378,41 +623,75 @@ impl Encryption for AesOperation {
         plain: &[u8],
         cipher: CK_BYTE_PTR,
         cipher_len: CK_ULONG_PTR,
-    ) -> KResult<()> {todo!()}
+    ) -> KResult<()> {
+        if cipher_len.is_null() {
+            return err_rv!(CKR_ARGUMENTS_BAD);
+        }
+        
+        let mut outlen = self.encryption_len(plain.len() as u64)?;
+        
+        if cipher.is_null() {
+            unsafe {
+                *cipher_len = outlen as CK_ULONG;
+            }
+            return Ok(());
+        }
 
-    fn encrypt_update(
-            &mut self,
-            _plain: &[u8],
-            _cipher: CK_BYTE_PTR,
-            _cipher_len: CK_ULONG_PTR,
-        ) -> KResult<()> {todo!()}
+        match self.mech {
+            #[cfg(False)]
+            CKM_AES_GCM => {
+                todo!()
+            }
+            _ => (),
+        }
+        if unsafe { *cipher_len as usize } < outlen {
+            /* This is the only, non-fatal error */
+            unsafe { *cipher_len = outlen as CK_ULONG };
+            return err_rv!(CKR_BUFFER_TOO_SMALL);
+        }
+        
+        let mut plain_buf = plain.as_ptr();
+        let mut plain_len = plain.len();
 
-    fn encrypt_final(
-            &mut self,
-            _cipher: CK_BYTE_PTR,
-            _cipher_len: CK_ULONG_PTR,
-        ) -> KResult<()> {todo!()}
+        let key = self.key.raw.as_slice();
+        let key: &Key<Aes256Gcm> = key.into();
+        let ctx = Aes256Gcm::new(key.into());
+        // Forcing to 12 bytes because of aes-gcm expects that
+        let nonce = &self.params.iv[..12];
+        let payload = plain;
+        let ciphertext = ctx.encrypt(nonce.into(),payload);
+        match ciphertext {
+            Ok(ct) => {
+                let ct_buf = ct.as_ptr();
+                let ct_len = ct.len();
+                // Safe to dereference cipher_len as we checked it's not null
+                let out_len = unsafe{*cipher_len as usize};
+                assert!(ct_len <= out_len);
+                unsafe 
+                {
+                std::ptr::copy_nonoverlapping(ct_buf, cipher, ct_len);
+                }
 
-    fn encryption_len(&self, _data_len: CK_ULONG) -> KResult<usize> {todo!()}
+            },
+            Err(e) => todo!("return error"),
+        }
+
+        Ok(())
+    }
+
+    fn encryption_len(&self, data_len: CK_ULONG) -> KResult<usize> {
+        let len: usize = match self.mech {
+            CKM_AES_GCM => data_len as usize + self.params.taglen,
+            _ => return err_rv!(CKR_GENERAL_ERROR),
+        };
+        Ok(len)
+    }
 }
 
 impl Decryption for AesOperation {
     fn decrypt(
             &mut self,
             _cipher: &[u8],
-            _plain: CK_BYTE_PTR,
-            _plain_len: CK_ULONG_PTR,
-        ) -> KResult<()> {todo!()}
-
-    fn decrypt_update(
-            &mut self,
-            _cipher: &[u8],
-            _plain: CK_BYTE_PTR,
-            _plain_len: CK_ULONG_PTR,
-        ) -> KResult<()> {todo!()}
-
-    fn decrypt_final(
-            &mut self,
             _plain: CK_BYTE_PTR,
             _plain_len: CK_ULONG_PTR,
         ) -> KResult<()> {todo!()}
